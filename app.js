34d45462a0ac9270de550453253ad42f6fa94bf8
de-cryptor <bim2014001@iiita.ac.in>
2019-12-24 11:30:11 +0530

    Mongoose Config in App

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+//const morgan = require('morgan');
 
 /* app.use((req,res,next) => {
     res.status(200).json({
@@ -14,6 +17,9 @@ const ProductRoutes = require('./api/routes/products')
 const OrderRoutes = require('./api/routes/orders')
 
 //app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
 
 app.use('/products', ProductRoutes);
 app.use('/orders', OrderRoutes);
